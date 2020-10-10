fc48c0d61bad56f59e7615d89a6e8b1d885f76ed
Simon <simon.bettens@live.be>
2020-10-10 19:58:43 +0200

    removed contact

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ProjectComponent } from './project/project.component';
 import { ProjectsOverviewComponent } from './projects-overview/projects-overview.component';
 import { MeComponent } from './me/me.component';
-import { ContactComponent } from './contact/contact.component';
 import { SkillsListComponent } from './skills-list/skills-list.component';
 import { SkillComponent } from './skill/skill.component';
 
 
 
 @NgModule({
-  declarations: [ProjectComponent,ProjectsOverviewComponent,MeComponent, ContactComponent, SkillsListComponent, SkillComponent],
+  declarations: [ProjectComponent,ProjectsOverviewComponent,MeComponent, SkillsListComponent, SkillComponent],
   imports: [
     CommonModule
   ],  
